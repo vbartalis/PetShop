41e679e685d0960243035a3040edc9d16183b629
vbartalis <vbartalis@gmail.com>
2021-10-03 14:51:02 +0300

    fixed reading constants from property

--- beckend/petshop/src/main/java/io/github/vbartalis/petshop/security/jwt/JwtConstants.java
+++ beckend/petshop/src/main/java/io/github/vbartalis/petshop/security/jwt/JwtConstants.java
@@ -2,17 +2,22 @@ package io.github.vbartalis.petshop.security.jwt;
 
 import io.jsonwebtoken.security.Keys;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 
+import javax.annotation.PostConstruct;
 import java.security.Key;
+import java.util.Objects;
 
 //todo might be better if these constants were not inside the code.
 
 /**
  * This class contains constants that are used by jwt tokens.
  */
+@Slf4j
 @Configuration
 @Data
 public class JwtConstants {
@@ -20,11 +25,20 @@ public class JwtConstants {
     @Autowired
     private Environment env;
 
-    private String secret = "TP21JJRfC+l26E7j4ZR31uvYabegKT4cfwosHoIKR64=";
-//    private String secret = env.getProperty("PetShop.JWT.Secret");
+    private String secret;
+    private Key key;
+    private long validityInMilliseconds;
 
-    private Key key = Keys.hmacShaKeyFor(secret.getBytes());
+    /**
+     * This method is used to read and initialize the constants from the property files.
+     * It is annotated with @PostConstruct so there is no need to call it manually.
+     */
+    @PostConstruct
+    public void init() {
+        this.secret = Objects.requireNonNull(env.getProperty("petshop.jwt.secret"));
+        this.key = Keys.hmacShaKeyFor(secret.getBytes());
+        this.validityInMilliseconds = Long.parseLong(Objects.requireNonNull(env.getProperty("petshop.jwt.validityinmilliseconds")));
+    }
 
-    private long validityInMilliseconds = 60 * 60 * 1000 * 24 * 7;  //7d
-//    private long validityInMilliseconds = env.getProperty("PetShop.JWT.ValidityInMilliseconds");
+
 }
