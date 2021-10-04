3bdb4fbfbce4ef0204d7722bd3b6084453fdf536
vbartalisdev <vbartalisdev@gmail.com>
2021-10-04 16:48:28 +0300

    finish login

--- frontend/pet-shop/src/app/modules/authentication/authentication-login/authentication-login.module.ts
+++ frontend/pet-shop/src/app/modules/authentication/authentication-login/authentication-login.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { AuthenticationLoginRoutingModule } from './authentication-login-routing.module';
 import { LoginComponent } from './login.component';
-
+import { SharedModule } from '@shared/shared.module';
 
 @NgModule({
-  declarations: [
-    LoginComponent
-  ],
-  imports: [
-    CommonModule,
-    AuthenticationLoginRoutingModule
-  ]
+  declarations: [LoginComponent],
+  imports: [CommonModule, AuthenticationLoginRoutingModule, SharedModule],
 })
-export class AuthenticationLoginModule { }
+export class AuthenticationLoginModule {}
--- frontend/pet-shop/src/app/modules/authentication/authentication-login/login.component.ts
+++ frontend/pet-shop/src/app/modules/authentication/authentication-login/login.component.ts
@@ -1,16 +1,75 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '@app/service/authentication.service';
+import { GlobalService } from '@app/service/global.service';
+import { Credentials } from '@data/model/credentials.model';
+import { LoginContext } from '@data/model/login-context';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  loading = false;
+  submitted = false;
+  error = '';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private authenticationService: AuthenticationService,
+    private globalService: GlobalService
+  ) {
+    // redirect if already logged in
+    if (this.globalService.isLoggedInAdmin()) {
+      this.router.navigate(['/admin']);
+    }
+    if (this.globalService.isLoggedInUser()) {
+      this.router.navigate(['/user']);
+    }
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
   }
 
+  ngOnInit(): void {}
+
+  // convenience getter for easy access to form fields
+  get f(): { [p: string]: AbstractControl } {
+    return this.loginForm.controls;
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    this.loading = true;
+    const loginContext = new LoginContext(this.f.username.value, this.f.password.value);
+    this.authenticationService
+      .login(loginContext)
+      // .pipe(first())
+      .subscribe(
+        (credentials: Credentials) => {
+          if (credentials.roles.includes('ROLE_USER')) {
+            this.router.navigate(['/user']);
+          } else if (credentials.roles.includes('ROLE_ADMIN')) {
+            this.router.navigate(['/admin']);
+          } else {
+            this.router.navigate(['/']);
+          }
+        },
+        (error) => {
+          this.error = JSON.stringify(error.error.message);
+          this.loading = false;
+        }
+      );
+  }
 }
